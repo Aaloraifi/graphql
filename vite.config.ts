5b6a69e4c2d32ed5054c3a9f222b8ed8fca49899
aaloraif <aysha.aloraifi1@gmail.com>
2025-06-12 23:21:00 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/graphql/', // 👈 use your repo name here
+  base: '/graphql/', // 👈 Replace with your GitHub repo name
+  // plugins: [react()],
 })
